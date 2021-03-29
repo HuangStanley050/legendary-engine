070ffa2ef7bb088d7128e76d10404f5d0c5d1a52
Stanley Huang <infamous_godhand@yahoo.com>
2021-03-29 11:00:39 +1100

    set up host

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("./node_modules/webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
@@ -6,6 +7,12 @@ module.exports = {
     port: 8080,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        products: "products@http://loalhost:8081/remoteEntry.js",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./dist/index.html",
     }),
