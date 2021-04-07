2ec6f53b6d066ed8d30a729d9c326dc65012308c
Stanley Huang <infamous_godhand@yahoo.com>
2021-04-08 09:04:44 +1000

    added cart app to container

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,5 @@
 import { mount } from "products/ProductsIndex";
-import "cart/CartShow";
+import { mount } from "cart/CartShow";
 console.log("container");
 mount(document.getElementById("my-products"));
+mount(document.getElementById("my-cart"));
